485f4e6b50ccbee479b0691e2a22b8839ed8a446
胡鹏 <64hp@163.com>
2018-06-16 17:40:22 +0800

    fix

---
+++ DYTrends/Classes/Controller/DYTrendsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DYTrendsViewController.swift
+//  Alamofire
+//
+//  Created by HuXuPeng on 2018/6/16.
+//
+
+import UIKit
+import NJKit
+
+class DYTrendsViewController: NJViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "关注"
+     
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+     
+    }
+
+}
--- DYTrends/Classes/Target_Trends.swift
+++ DYTrends/Classes/Target_Trends.swift
@@ -6,10 +6,20 @@
 //
 
 import UIKit
-//import NJTrendsLiveShowViewController
+import NJKit
 
 @objc class Target_Trends: NSObject {
-    @objc func Action_mainViewController(params: [String: AnyObject]) -> UIViewController {
-        return NJTrendsLiveShowViewController()
+
+    @objc func Action_mainViewController(params: [String: AnyObject]) -> UIViewController? {
+        
+        let childController = DYTrendsViewController()
+        let nav = NJNavigationController(rootViewController: childController)
+        
+        childController.tabBarItem.image = UIImage.nj_imageFromXcassets(name: "btn_live_normal", bundleClass: self.classForCoder)
+        childController.tabBarItem.selectedImage = UIImage.nj_imageFromXcassets(name: "btn_live_selected", bundleClass: self.classForCoder)
+        childController.tabBarItem.title = "tabBartitle"
+        childController.tabBarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -3);
+        
+        return nav
     }
 }
