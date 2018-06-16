511f1d759d9709aaa5e26ead6d6f7c4248ae1e3f
胡鹏 <64hp@163.com>
2018-06-16 18:27:45 +0800

    fix

--- DYTrends/Classes/Controller/DYTrendsViewController.swift
+++ DYTrends/Classes/Controller/DYTrendsViewController.swift
@@ -13,7 +13,7 @@ class DYTrendsViewController: NJViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "关注"
-     
+        nj_isBackActionBtnHidden = true
     }
 
     override func didReceiveMemoryWarning() {
